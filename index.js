2deed4fae068528afca6a15c8bf3362e61f7b1e1
Rodrigo Ali <rodrigojali@gmail.com>
2017-03-06 20:37:28 +0000

    Added index.html and index.js; should test it in Heroku

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var express = require("express");
+var moment = require("moment");
+var path = require("path");
+
+var app = express();
+
+app.set("path", (process.env.PORT || 8080));
+
+app.use(express.static(path.resolve(__dirname, "client")));
+
+app.get('/', function(req, res) {
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+app.get('/:datestring', function(req,res) {
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+});
+
+app.listen(app.get("port"), function() {
+  console.log(`Server listening on port ${app.get("port")}`);
+});
