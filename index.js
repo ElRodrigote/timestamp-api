92df09ae2e715340c825e0562db059e62c152fbf
Rodrigo Ali <rodrigojali@gmail.com>
2017-03-07 21:13:00 +0000

    Some changes made, trying to get app running in heroku

--- index.js
+++ index.js
@@ -1,48 +1,31 @@
 'use strict';
 
-var express = require("express");
-var moment = require("moment");
-var path = require("path");
-
+var path = require('path');
+var moment = require('moment');
+var express = require('express');
 var app = express();
 
-app.set("path", (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(path.resolve(__dirname, "client")));
-
-app.get('/', function(req, res) {
-  var fileName = path.join(__dirname, 'index.html');
-  res.sendFile(fileName, function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Sent:', fileName);
-    }
+app.use(express.static(path.resolve(__dirname, 'client')));
+app.get('/:timestamp', (req,res) => {
+  var time = moment(req.params.timestamp, 'MMMM DD, YYYY', true);
+  if (!time.isValid())
+    time = moment.unix(req.params.timestamp);
+  
+  if (!time.isValid()) {
+    res.json({
+      'humanReadable': null,
+      'unix': null
+    });
+  }
+  
+  res.json({
+    'humanReadable': time.format('MMMM DD, YYYY'),
+    'unix': time.format('X')
   });
 });
 
-app.get('/:datestring', function(req,res) {
-  var myDate;
-  if(/^\d{8,}$/.test(req.params.datestring)) {
-    myDate = moment(req.params.datestring, "X");
-  } else {
-    myDate = moment(req.params.datestring, "MMMM D, YYYY");
-  }
-  if(myDate.isValid()) {
-    res.json({
-      unix: myDate.format("X"),
-      natural: myDate.format("MMMM D, YYYY")
-    });
-  } else {
-    res.json({
-      unix: null,
-      natural: null
-    });
-  }
-});
-
-app.listen(app.get("port"), function() {
-  console.log(`Server listening on port ${app.get("port")}`);
+app.listen(app.get('port'), () => {
+  console.log(`Server listening on port ${app.get('port')}`);
 });
