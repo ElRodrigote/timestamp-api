f7e5fe7e0487805e63aa8d6d971ffe908ec82d1f
Rodrigo Ali <rodrigojali@gmail.com>
2017-03-08 15:41:40 +0000

    Final changes to server.js script, trial

--- server.js
+++ server.js
@@ -3,34 +3,33 @@
 var path = require('path');
 var moment = require('moment');
 var express = require('express');
-var app = express();
-var port = process.env.PORT || 3500;
 
-app.listen(port, function(){
-  console.log("Listening on port: " + port);
-});
+var app = express();
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-  var fileName = path.join(__dirname, 'index.html');
-  res.sendFile(fileName, function (err) {
+  var file = path.join(__dirname, 'index.html');
+  res.sendFile(file, function (err) {
     if (err) {
       console.log(err);
       res.status(err.status).end();
     }
     else {
-      console.log('Sent:', fileName);
+      console.log('Sent: ', file);
     }
   });
 });
 
 app.get('/:datestring', function(req,res) {
   var myDate;
+  //we ask for the parameter's nature: if it is an unix number or date string
   if(/^\d{8,}$/.test(req.params.datestring)) {
-    myDate = moment(req.params.datestring, "X");
+    myDate = moment(req.params.datestring, "X"); //parse to UNIX
   } else {
-    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+    myDate = moment(req.params.datestring, "MMMM D, YYYY"); //parse to Natural 
   }
 
+  // we set the response json to the formated timestamp or null if it's not a valid moment date
   if(myDate.isValid()) {
     res.json({
       unix: myDate.format("X"),
@@ -42,6 +41,8 @@ app.get('/:datestring', function(req,res) {
       natural: null
     });
   }
+});
 
-
+app.listen(port, function(){
+  console.log("Listening to port: " + port);
 });
